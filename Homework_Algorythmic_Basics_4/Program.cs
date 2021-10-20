fd2e26b26061ba6de6e2eb18fc9894b739ce25a5
Hetenyi, Zoltan <zoltan.hetenyi@semilab.hu>
2021-10-20 22:53:25 +0200

    4th task complete, v1.0

---
+++ Homework_Algorythmic_Basics_4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Homework_Algorythmic_Basics_4
+{
+    class Program
+    {
+        //Végy fel egy 10 elemű, egész számokat tartalmazó tömböt.Töltsd fel random számokkal.
+        //Írd ki mindegyik elemről, hogy páros, vagy páratlan.
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 elemű tömb feltöltése random számokkal:");
+            int[] randomArray = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                randomArray[i] = rnd.Next(-2000000, 2000000);
+                Console.WriteLine(randomArray[i]);
+            }
+            Console.WriteLine("Listázzuk a tömbben lévő PÁROS számokat:");
+            Console.ReadKey();
+            foreach (var item in randomArray)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                    Thread.Sleep(400);
+                }
+            }
+            Console.WriteLine("Listázzuk a tömbben lévő PÁRATLAN számokat:");
+            foreach (var item in randomArray)
+            {
+                if (item % 2 != 0)
+                {
+                    Console.WriteLine(item);
+                    Thread.Sleep(400);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
