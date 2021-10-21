b872027b673cf2d66009224029450d69447472d3
Hetenyi, Zoltan <zoltan.hetenyi@semilab.hu>
2021-10-21 19:46:10 +0200

    6th task complete, v1.0

---
+++ HomeWork_Algorhythmic_Basics_6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace HomeWork_Algorhythmic_Basics_6
+{
+    class Program
+    {
+        //0-3000 írd ki a szökő éveket
+        //Szökőév minden olyan év, amelynek az évszáma néggyel osztható, kivéve a 100-zal osztható dátumok. A kivétel alóli kivételt a 400-zal osztható dátumok adják,
+        //vagyis például 2000 is szökőév volt, ám 2100 már nem lesz az.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A szökőévek 0-3000 között:");
+            for (int i = 1; i < 3001; i++)
+            {
+
+                if (i % 4 == 0 && i < 100)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(400);
+                    continue;
+                }
+                else if (i % 4 == 0)
+                {
+                    if (i % 100 == 0 && i % 400 == 0 )
+                    {
+                        Console.WriteLine(i);
+                        Thread.Sleep(400);
+                        continue;
+                    }
+                    if (i % 100 == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(i);
+                    Thread.Sleep(400);
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
