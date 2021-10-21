2e2b582ebed9bb3eeb8e114fac2eee13523a4ab9
Hetenyi, Zoltan <zoltan.hetenyi@semilab.hu>
2021-10-21 02:32:25 +0200

    minor fixes, v0.6

--- HomeWork_Algorhythmic_Basics_5/Program.cs
+++ HomeWork_Algorhythmic_Basics_5/Program.cs
@@ -12,7 +12,7 @@ namespace HomeWork_Algorhythmic_Basics_5
         static void Main(string[] args)
         {
             short k = 0;
-            for (short i = 0; i < 31; i++)
+            for (short i = 0; i < 543; i++)
             {
                 if (i <= 1)
                     continue;
@@ -20,15 +20,16 @@ namespace HomeWork_Algorhythmic_Basics_5
                 {
                     Console.WriteLine(i);
                 }
-                else if (i % 2 == 0)
+                else if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
                 {
                     continue;
                 }
                 else
-                { 
+                {
+                    //A range-ben minden számot ellenőrizni, hogy osztható-e a "2...2 négyzetnének" tartomány bármely számával.
                     for (short j = 2; j * j <= i; j++)
                     {
-                        // If a divisor of n exists
+                        //ha van osztó, a maradék nem lehet 0
                         if (i % j != 0)
                         {
                             if (k == i)
